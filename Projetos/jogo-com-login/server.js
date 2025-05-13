c6a394bb9e330a141a246fd8fbd0645d85a5dc12
Eric Meleti <ericmeleti2020@gmail.com>
2025-05-13 19:26:30 -0300

    node

---
+++ Projetos/jogo-com-login/server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(session({
+  secret: 'segredo-super-secreto',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(express.urlencoded({ extended: true }));
+
+// Servir apenas arquivos estáticos (JS, CSS, imagens)
+app.use('/js', express.static(path.join(__dirname, 'public/js')));
+app.use('/css', express.static(path.join(__dirname, 'public/css')));
+
+// Página de login
+app.get('/', (req, res) => {
+  if (req.session.logado) {
+    res.redirect('/jogo');
+  } else {
+    res.sendFile(path.join(__dirname, 'views', 'login.html'));
+  }
+});
+
+// Rota de autenticação
+app.post('/login', (req, res) => {
+  const { usuario, senha } = req.body;
+  if (usuario === 'admin' && senha === '1234') {
+    req.session.logado = true;
+    res.redirect('/jogo');
+  } else {
+    res.send('<p>Login inválido. <a href="/">Tentar novamente</a></p>');
+  }
+});
+
+// Jogo protegido
+app.get('/jogo', (req, res) => {
+  if (req.session.logado) {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  } else {
+    res.redirect('/');
+  }
+});
+
+// Logout
+app.get('/logout', (req, res) => {
+  req.session.destroy(() => {
+    res.redirect('/');
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
